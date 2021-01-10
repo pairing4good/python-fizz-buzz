bd989e2a5d6bb76742ac7ff1d6a30967572521a6
dkessler <dkessler@pillartechnology.com>
2021-01-10 15:22:25 -0600

    all the things

--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -10,5 +10,8 @@ class FizzBuzzTest(unittest.TestCase):
     def test_should_return_fizz_when_provided_with_three(self):
         self.assertEqual('fizz', fizzbuzz(3))
 
+    def test_should_return_fizz_when_provided_with_five(self):
+        self.assertEqual('buzz', fizzbuzz(5))
+
 if __name__ == '__main__':
     unittest.main()
